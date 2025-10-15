3a680d850697f49f372084702c28f200b52e7c76
Pratham <Pratham@Astrotalks-MacBook-Pro.local>
2025-10-15 10:55:49 +0530

    Initial project commit

---
+++ src/test/java/pratham/dhvani/DhvaniApplicationTests.java
@@ -0,0 +1,13 @@
+package pratham.dhvani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhvaniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
