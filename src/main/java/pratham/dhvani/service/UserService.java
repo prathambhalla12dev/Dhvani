d8dfb386f25177b855e21902ac5b0a4e96dc6439
Pratham <Pratham@Astrotalks-MacBook-Pro-2.local>
2025-10-18 21:49:57 +0530

    done

--- src/main/java/pratham/dhvani/service/UserService.java
+++ src/main/java/pratham/dhvani/service/UserService.java
@@ -1,9 +1,17 @@
 package pratham.dhvani.service;
 
 import org.springframework.stereotype.Service;
+import pratham.dhvani.dto.ApiResponseDto;
+import pratham.dhvani.dto.UserSignupRequestDto;
 
 @Service
 public class UserService {
 
+    public ApiResponseDto signup(UserSignupRequestDto userSignupRequestDto) {
+        ApiResponseDto response = new ApiResponseDto();
 
+
+
+        return response;
+    }
 }
